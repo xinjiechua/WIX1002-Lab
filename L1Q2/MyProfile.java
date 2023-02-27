9215f4d8fd0e20253b06bbe7720f4a79e9737215
xinjiechua <122278615+xinjiechua@users.noreply.github.com>
2023-02-27 18:31:08 +0800

    Delete L1Q2 directory

--- L1Q2/MyProfile.java
+++
@@ -1,9 +0,0 @@
-package L1Q2;
-
-public class MyProfile {
-    public static void main(String[] args) {
-
-        System.out.println("My Profile: \nFull Name: Chua Xin Jie\nMatric Number: 22004826" +
-                "\nAddress: KK8 \nEmail:22004826@siswa.um.edu.my\nContact:0164028306");
-    }
-}
